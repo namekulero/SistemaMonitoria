f956697e90102c4df63217c6c6c84598289e7a61
Namekulero <jmontero.gutierrez2002@gmail.com>
2023-10-25 09:01:29 -0500

    9:01 am

---
+++ Servidor/src/Clases/Cita.java
@@ -0,0 +1,11 @@
+package Clases;
+
+public class Cita {
+    String citaId;
+    String estudianteId;
+
+    public Cita(String citaId, String estudianteId) {
+        this.citaId = citaId;
+        this.estudianteId = estudianteId;
+    }
+}
